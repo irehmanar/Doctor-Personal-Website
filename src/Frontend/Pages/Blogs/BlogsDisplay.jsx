f7d9e5db9b8a373ce06b5c96ca7e3ad7e7201f03
Muhammad Ismaeel Zeb <109311287+ismaeel06@users.noreply.github.com>
2024-05-11 02:25:45 +0500

    Fixed some Responsiveness Issues in the Navbar

--- src/Frontend/Pages/Blogs/BlogsDisplay.jsx
+++ src/Frontend/Pages/Blogs/BlogsDisplay.jsx
@@ -4,21 +4,16 @@ import Card from '../../Components/Blogs/BlogPostCard.jsx'
 
 function Blogs() {
   return (
-    <div>
       <div style={{backgroundColor: "#F5FBFF"}}>
-            <Navbar />
+      <Navbar />
             <h1 className='py-8 text-center text-3xl font-bold font-sans'>Read your Nutritionist's Blogs Here</h1>
             <div className='flex flex-col gap-8'>
-            <Card />
-            <Card />
-            <Card />
             <Card />
             <Card />
             <Card />
             </div>
             
         </div>
-    </div>
   )
 }
 
