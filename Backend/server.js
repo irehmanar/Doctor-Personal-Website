209772ecbb09e47c2ac93acc13137d676566f96f
abdasg25 <abrehman.bscs22seecs@seecs.edu.pk>
2024-05-14 21:32:36 +0500

    Removed error from server.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,8 +4,7 @@ import userRoutes from "./routes/userRoutes.js";
 import promotionRoutes from "./routes/promotionRoute.js";
 import appointmentRoutes from "./routes/appointmentRoutes.js";
 import adminDashboardRoute from "./routes/adminDashboardRoutes.js";
-import therapeuticPromotionRoute from "./routes/therapeuticRoutes.js";
-import premiumPromotionRoute from "./routes/premiumRoutes.js";
+
 import cors from "cors";
 const app = express();   
 const port = 3333;
@@ -18,14 +17,6 @@ app.use('/hospital',userRoutes);
 app.use('/promotion',promotionRoutes);
 app.use('/appointment',appointmentRoutes);
 app.use('/adminDashboard',adminDashboardRoute);
-app.use('/therapeuticPromotion',therapeuticPromotionRoute);
-app.use('/premiumPromotion',premiumPromotionRoute);
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
 });
-
-
-
-
-
-
