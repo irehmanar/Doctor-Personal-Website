0b504d9aae2e2c3d71656250d16e7f4170484485
abdasg25 <abrehman.bscs22seecs@seecs.edu.pk>
2024-05-18 22:04:35 +0500

    added api

--- Backend/routes/adminDashboardRoutes.js
+++ Backend/routes/adminDashboardRoutes.js
@@ -8,5 +8,5 @@ adminDashboardRoute.get('/viewAppointment',adminDashboardController.viewappointm
 adminDashboardRoute.put('/acceptAppointment/:Id',adminDashboardController.acceptAppointment); //need changes
 adminDashboardRoute.get('/patientPage',adminDashboardController.patientPage); //done
 adminDashboardRoute.get('/packagePage',adminDashboardController.packagePage); //done
-// adminDashboardRoute.get('/patientData',adminDashboardController.getLatestAppointments);
+adminDashboardRoute.get('/patientData',adminDashboardController.getLatestAppointments);
 export default adminDashboardRoute
